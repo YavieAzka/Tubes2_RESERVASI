26a0e766d320042d58e2b790d3c6d6135f0b3115
Yavie Azka Putra Araly <134411822+YavieAzka@users.noreply.github.com>
2026-04-25 20:18:33 +0700

    Delete src directory

--- src/test_scraper.ts
+++
@@ -1,8 +0,0 @@
-import { scrapeHTML } from "./scraper";
-
-async function main() {
-  const html = await scrapeHTML("https://example.com");
-  console.log(html.substring(0, 500)); // Tampilkan 500 karakter pertama
-}
-
-main();
